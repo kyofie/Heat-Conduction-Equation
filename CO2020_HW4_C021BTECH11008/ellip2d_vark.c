8a52c6a165f04ad3ae7e7af9b6a95e3f8b6fd36b
kyofie <kondapalliyoshita@gmail.com>
2023-06-20 13:25:16 +0530

    Initial commit

---
+++ CO2020_HW4_C021BTECH11008/ellip2d_vark.c
@@ -0,0 +1,505 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+const double PI = 4.0*atan(1.0);
+
+void grid(int nx, double xst, double xen, double *x, double *dx)
+{
+  int i;
+  double dxunif;
+
+  // uniform mesh for now; 
+  // can use stretching factors to place x nodes later
+  dxunif = (xen-xst)/(double)(nx-1);
+
+  // populate x[i] s
+  for(i=0; i<nx; i++)
+    x[i] = xst + ((double)i)  * dxunif;
+
+  // dx[i] s are spacing between adjacent xs
+  for(i=0; i<nx-1; i++)
+    dx[i] = x[i+1]-x[i];
+
+}
+
+void set_initial_guess(int nx, int ny, double *x, double *y, double **T)
+{
+  int i, j;
+
+  for(i=0; i<nx; i++)
+    for(j=0; j<ny; j++)
+      T[i][j] = 0.0;
+}
+
+void calc_diffusivity(int nx, int ny, double *x, double *y, double **T, double **kdiff)
+{
+  int i, j;
+  double A = 1.0, B = 0.8;
+
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+       kdiff[i][j] = A+B*T[i][j];
+   }
+}
+
+void calc_sources(int nx, int ny, double *x, double *y, double *dx, double *dy, double **T, double **b)
+{
+  int i, j;
+
+  // calculate source (may be dependent on T)
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+     b[i][j] = -((1 + 0.8 * x[i] * (1-x[i]) * cos(PI*y[j])) * (2 * cos(PI * y[j]) - x[i] * (x[i] - 1) * PI * PI * cos(PI*y[j])) - 0.8 * ((1 - 2 * x[i] * cos(PI * y[j])) *  (1 - 2 * x[i] * cos(PI * y[j])) + (x[i] * (x[i] - 1) * PI * sin(PI * y[j])) * (x[i] * (x[i] - 1) * PI * sin(PI * y[j]))));                      // set source function here
+     b[i][j] = -b[i][j] * dx[0] * dx[0]; // only works for uniform mesh
+   }
+}
+
+void set_boundary_conditions(int nx, int ny, double *x, double *y, double *dx, double *dy, double **T, double **bcleft, double **bcrght, double **bctop, double **bcbot)
+{
+
+  int i, j;
+  double bcE, bcW, bcN, bcS, kw, ke, kn, ks;
+
+  // left boundary -- Homogeneous Dirichlet
+  // q(y) * T + p(y) * dTdx = r(y)
+  for(j=0; j<ny; j++)
+  {
+    bcleft[j][0] = 0.0;  // p(y)
+    bcleft[j][1] = 1.0;  // q(y)
+    bcleft[j][2] = 0.0;  // r(y)
+  }
+
+  // right boundary -- Homogeneous Dirichlet
+  // q(y) * T + p(y) * dTdx = r(y)
+  for(j=0; j<ny; j++)
+  {
+    bcrght[j][0] = 0.0;  // p(y)
+    bcrght[j][1] = 1.0;  // q(y)
+    bcrght[j][2] = 0.01;  // r(y)
+  }
+
+  // bottom boundary -- Homogeneous  Neumann
+  // q(x) * T + p(x) * dTdy = r(x)
+  for(i=1; i<nx-1; i++)
+  {
+    bcbot[i][0]= 1.0;
+    bcbot[i][1]= 0.0;
+    bcbot[i][2]= 0.0;
+  }
+
+  // top boundary -- Homogeneous Dirichlet
+  // q(x) * T + p(x) * dTdy = r(x)
+  for(i=1; i<nx-1; i++)
+  {
+    bctop[i][0]= 0.0;
+    bctop[i][1]= 1.0;
+    bctop[i][2]= 0.0;
+  }
+}
+
+void get_coeffs(int nx, int ny, double *x, double *y, double *dx, double *dy, double **aP, double **aE, double **aW, double **aN, double **aS, double **b, double **T, double **kdiff, double **bcleft, double **bcrght, double **bctop, double **bcbot)
+{
+
+  int i, j;
+  double pj, qj, rj;
+  double hxhy_sq;
+
+  // calculate diffusivity at [x, y]; may be dependent on T
+  calc_diffusivity(nx, ny, x, y, T, kdiff);
+
+  // calculate sources Su, Sp at [x, y]; may be dependent on T
+  calc_sources(nx, ny, x, y, dx, dy, T, b);
+
+  // populate values in BC arrays
+  set_boundary_conditions(nx, ny, x, y, dx, dy, T, bcleft, bcrght, bctop, bcbot);
+ 
+  // start populating the coefficients
+  hxhy_sq = dx[0]*dx[0]/(dy[0]*dy[0]);
+
+  // ------ Step 1 :: interior points ------
+  for(i=1; i<nx-1; i++)
+   for(j=1; j<ny-1; j++)
+   {
+     double kdiffy = (kdiff[i][j+1]-kdiff[i][j-1])/(2*dy[0]);
+     double kdiffx = (kdiff[i+1][j]-kdiff[i-1][j])/(2*dx[0]);
+     aP[i][j] = 2.0 * kdiff[i][j] * (1 + hxhy_sq);
+     aE[i][j] = - kdiff[i][j] + (dx[0] * kdiffx)/2;   aW[i][j] = - kdiff[i][j] + (dx[0] * kdiffx)/2;
+     aN[i][j] = -hxhy_sq * (kdiff[i][j] + (kdiffy* dy[0])/2);   aS[i][j] = -hxhy_sq * (kdiff[i][j] - (kdiffy * dy[0])/2);
+   }
+
+  // ------ Step 2 :: left boundary ----------
+  i = 0;
+   for(j=0; j<ny; j++)
+   {
+     pj = bcleft[j][0]; qj = bcleft[j][1]; rj = bcleft[j][2];
+     double kdiffy;
+     if(j == 0){
+       //forward difference
+       kdiffy = (kdiff[i][j+1]-kdiff[i][j])/dy[0];
+     }
+     else if(j == ny-1){
+       //backward differnce
+       kdiffy = (kdiff[i][j]-kdiff[i][j-1])/dy[0];
+     }
+     else{
+       kdiffy = (kdiff[i][j+1]-kdiff[i][j-1])/(2*dy[0]);
+     }
+     //forward difference
+     double kdiffx = (kdiff[i+1][j]-kdiff[i][j])/dx[0];
+  
+     aP[i][j] = 2.0 * kdiff[i][j] * (1 + hxhy_sq) * pj - (kdiff[i][j] - kdiffx * (dx[0]/2))*(2 * dx[0] * qj);
+     aE[i][j] = -2.0 * pj *(kdiff[i][j] + kdiffx * (dx[0]/2));       aW[i][j] = 0.0;
+     aN[i][j] = -hxhy_sq * pj *(kdiff[i][j] + kdiffy * (dy[0]/2));   aS[i][j] = -hxhy_sq * pj * (kdiff[i][j] - kdiffy * (dy[0]/2));
+     b[i][j] = b[i][j] * pj - 2.0 * dx[0] * rj * (kdiff[i][j]-kdiffx * (dx[0]/2));
+   }
+  // ------ Step 2 :: left boundary done ---
+
+  // ------ Step 3 :: right boundary ----------
+  i = nx-1;
+   for(j=0; j<ny; j++)
+   {
+     pj = bcrght[j][0]; qj = bcrght[j][1]; rj = bcrght[j][2];
+     double kdiffy;
+     if(j == 0){
+       //forward difference
+       kdiffy = (kdiff[i][j+1]-kdiff[i][j])/dy[0];
+     }
+     else if(j == ny-1){
+       //backward differnce
+       kdiffy = (kdiff[i][j]-kdiff[i][j-1])/dy[0];
+     }
+     else{
+       kdiffy = (kdiff[i][j+1]-kdiff[i][j-1])/(2*dy[0]);
+     }
+     //backward difference
+     double kdiffx = (kdiff[i][j]-kdiff[i-1][j])/dy[0];
+     aP[i][j] = 2.0 * kdiff[i][j] * (1 + hxhy_sq) * pj + (kdiff[i][j] + kdiffx * (dx[0]/2))*(2 * dx[0] * qj);
+     aW[i][j] = 2.0 * kdiff[i][j] * pj;   aE[i][j] = 0.0;
+     aN[i][j] = -hxhy_sq * pj *(kdiff[i][j] + kdiffy * (dy[0]/2));   aS[i][j] = -hxhy_sq * pj * (kdiff[i][j] - kdiffy * (dy[0]/2));
+     b[i][j] = b[i][j] * pj + 2.0 * dx[0] * rj * (kdiff[i][j] + kdiffx * (dx[0]/2));
+   }
+  // ------ Step 3 :: right boundary done ---
+ 
+  // ------ Step 4 :: bottom boundary ----------
+  j = 0;
+  for(i=1; i<nx-1; i++)
+   {
+     pj = bcbot[i][0]; qj = bcbot[i][1]; rj = bcbot[i][2];
+     //forward difference
+     double kdiffy = (kdiff[i][j+1]-kdiff[i][j])/dx[0];
+     double kdiffx = (kdiff[i+1][j]-kdiff[i-1][j])/(2*dy[0]);
+     aP[i][j] = 2.0 * kdiff[i][j] * (1 + hxhy_sq) * pj - hxhy_sq * (kdiff[i][j] - kdiffy * (dy[0]/2)) * 2.0 * dy[0] *qj;
+     aW[i][j] = - pj * (kdiff[i][j] + kdiffy * (dx[0]/2));   aE[i][j] = - pj * (kdiff[i][j] - kdiffy * (dx[0]/2));
+     aN[i][j] = -2.0 * hxhy_sq * kdiff[i][j] * pj;   aS[i][j] = 0.0;
+     b[i][j] = b[i][j] * pj - hxhy_sq * (kdiff[i][j] - kdiffy * (dy[0]/2)) * 2.0 * dy[0] * rj;
+   }
+  // ------ Step 4 :: bottom boundary done ---
+
+  // ------ Step 5 :: top boundary ----------
+  j = ny-1;
+  for(i=1; i<nx-1; i++)
+   {
+     pj = bctop[i][0]; qj = bctop[i][1]; rj = bctop[i][2];
+     //backward difference
+     double kdiffy = (kdiff[i][j]-kdiff[i][j-1])/dx[0];
+     double kdiffx = (kdiff[i+1][j]-kdiff[i-1][j])/(2*dy[0]);
+     aP[i][j] = 2.0 * kdiff[i][j] * (1 + hxhy_sq) * pj + hxhy_sq * (kdiff[i][j] + kdiffy * (dy[0]/2)) * 2.0 * dy[0] *qj;
+     aW[i][j] = - pj * (kdiff[i][j] + kdiffy * (dx[0]/2));   aE[i][j] = - pj * (kdiff[i][j] - kdiffy * (dx[0]/2));
+     aN[i][j] = 0.0;   aS[i][j] = -2.0 * hxhy_sq * kdiff[i][j] * pj;
+     b[i][j] = b[i][j] * pj + hxhy_sq * (kdiff[i][j] + kdiffy * (dy[0]/2)) * 2.0 * dy[0] * rj;
+   }
+  // ------ Step 5 :: top boundary done ---
+
+
+}
+
+double get_max_of_array(int nx, int ny, double **arr)
+{
+  int i, j;
+  double arrmax, val;
+
+  arrmax = arr[0][0];
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+     val = fabs(arr[i][j]);
+     if(arrmax < val)
+       arrmax = val;
+   }
+  return arrmax;
+}
+
+double get_l2err_norm(int nx, int ny, double **arr1, double **arr2)
+{
+  double l2err = 0.0, val;
+  int i, j;
+
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+     val = arr1[i][j] - arr2[i][j];
+     l2err += val*val;
+   }
+
+  l2err = l2err/((double) (nx*ny));
+  l2err = sqrt(l2err);
+
+  return l2err;
+}
+
+void solve_gssor(int nx, int ny, double *x, double *y, double *dx, double *dy, double **aP, double **aE, double **aW, double **aN, double **aS, double **b, double **T, double **Tpad, double **Tpnew, int max_iter, double tol, double relax_T, double **kdiff, double **bcleft, double **bcrght, double **bctop, double **bcbot)
+{
+
+  int i, j, ip, jp, iter;
+  double l2err, arrmax1, arrmax2, err_ref, T_gs, rel_err;
+  
+  double **Tprev;
+  Tprev = (double **)malloc(nx * sizeof(double *));
+  for(i=0; i<nx; i++) Tprev[i] = (double *)malloc(ny * sizeof(double));
+
+  // copy to padded array
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+      ip = i+1; jp = j+1;
+      Tpad[ip][jp] = T[i][j];
+   }
+
+ // now perform iterations
+  for(iter=0; iter<max_iter; iter++)
+  {
+    // update Tpnew
+    for(i=0; i<nx; i++)
+     for(j=0; j<ny; j++)
+     {
+       Tprev[i][j] = T[i][j];
+       ip = i+1; jp = j+1;
+       T_gs = ( b[i][j] - aE[i][j]*Tpnew[ip+1][jp] - aW[i][j]*Tpnew[ip-1][jp] - 
+                          aN[i][j]*Tpnew[ip][jp+1] - aS[i][j]*Tpnew[ip][jp-1] ) / aP[i][j];
+       Tpnew[ip][jp] = (1.0-relax_T)*Tpad[ip][jp] + relax_T*T_gs;
+
+     }
+
+    // check for convergence
+    l2err = get_l2err_norm(nx+2, ny+2, Tpad, Tpnew);
+    arrmax1 = get_max_of_array(nx+2, ny+2, Tpad );
+    arrmax2 = get_max_of_array(nx+2, ny+2, Tpnew);
+    err_ref = fmax(arrmax1, arrmax2);  err_ref = fmax(err_ref, 1.0e-6);
+    rel_err = l2err/err_ref;
+    
+    if(rel_err < tol)
+      break;
+
+    // prepare for next iteration
+    for(i=0; i<nx; i++)
+     for(j=0; j<ny; j++)
+     {
+       ip = i+1; jp = j+1;
+       Tpad[ip][jp] = Tpnew[ip][jp];
+     }
+
+  }
+
+  // copy from padded array
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+   {
+      ip = i+1; jp = j+1;
+      T[i][j] = Tpad[ip][jp];
+   }
+   
+   if(get_l2err_norm(nx, ny, Tprev, T)>tol){
+   	get_coeffs(nx, ny, x, y, dx, dy, aP, aE, aW, aN, aS, b, T, kdiff, bcleft, bcrght, bctop, bcbot);
+   	solve_gssor(nx, ny, x, y, dx, dy, aP, aE, aW, aN, aS, b, T, Tpad, Tpnew, max_iter, tol, relax_T, kdiff, bcleft, bcrght, bctop, bcbot);
+   	 
+   }
+
+
+  for(i=0; i<nx; i++) free(Tprev[i]);
+  free(Tprev);
+}
+
+void get_exact_soln(int nx, int ny, double *x, double *y, double **Tex)
+{
+  int i, j;
+
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+     Tex[i][j] = x[i]*(1-x[i])*cos(PI*y[j]);
+}
+
+void output_soln(int nx, int ny, int iter, double *x, double *y, double **T, double **Tex)
+{
+  int i, j;
+  FILE* fp;
+  char fname[100];
+
+  sprintf(fname, "T_xy_vark_%03d_%03d_%04d.dat", nx, ny, iter);
+
+  fp = fopen(fname, "w");
+  for(i=0; i<nx; i++)
+   for(j=0; j<ny; j++)
+     fprintf(fp, "%lf %lf %lf %lf\n", x[i], y[j], T[i][j], Tex[i][j]);
+  fclose(fp);
+
+}
+
+int main()
+{
+
+  int nx, ny;
+  double *x, *dx, *y, *dy;
+  double ** aP, **aE, **aW, **aN, **aS, **b, **Sp;
+  double **T, **Tex, **kdiff, **wrk1, **wrk2;
+  double xst, xen, yst, yen;
+  double **bcleft, **bcrght, **bctop, **bcbot;
+  int i, j, max_iter;
+  double relax_T, tol, l2err;
+  FILE* fp;  
+
+  // read inputs
+  fp = fopen("input.in", "r");
+  fscanf(fp, "%d %d\n", &nx, &ny);
+  fscanf(fp, "%lf %lf\n", &xst, &xen);
+  fscanf(fp, "%lf %lf\n", &yst, &yen);
+  fclose(fp);
+
+ 
+
+  // allocate memory
+ 
+  x  = (double *)malloc(nx     * sizeof(double));   // grid points
+  dx = (double *)malloc((nx-1) * sizeof(double));   // spacing betw grid points
+
+  y  = (double *)malloc(ny     * sizeof(double));   // grid points
+  dy = (double *)malloc((ny-1) * sizeof(double));   // spacing betw grid points
+
+
+
+  // allocate 2D arrays dynamically
+  // -- for T --
+  T = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    T[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for Tex --
+  Tex = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    Tex[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for aP --
+  aP = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    aP[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for aE --
+  aE = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    aE[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for aW --
+  aW = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    aW[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for aN --
+  aN = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    aN[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for aS --
+  aS = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    aS[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for b --
+  b = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    b[i] = (double *)malloc(ny*sizeof(double));
+
+  // left boundary condition
+  bcleft  = (double **)malloc( ny * sizeof(double*));
+  for(i=0; i<ny; i++)
+    bcleft[i] = (double *)malloc(3*sizeof(double));
+
+  // right boundary condition
+  bcrght  = (double **)malloc( ny * sizeof(double*));
+  for(i=0; i<ny; i++)
+    bcrght[i] = (double *)malloc(3*sizeof(double));
+
+  // bottom boundary condition
+  bctop   = (double **)malloc( nx * sizeof(double*));
+  for(i=0; i<nx; i++)
+    bctop[i] = (double *)malloc(3*sizeof(double));
+
+  // top boundary condition
+  bcbot   = (double **)malloc( nx * sizeof(double*));
+  for(i=0; i<nx; i++)
+    bcbot[i] = (double *)malloc(3*sizeof(double));
+
+  // -- for kdiff --
+  kdiff = (double **)malloc(nx*sizeof(double *));
+  for(i=0; i<nx; i++)
+    kdiff[i] = (double *)malloc(ny*sizeof(double));
+
+  // -- for work arrays wrk1, wrk2 --
+  wrk1 = (double **)malloc((nx+2)*sizeof(double *));
+  for(i=0; i<nx+2; i++)
+    wrk1[i] = (double *)malloc((ny+2)*sizeof(double));
+
+  wrk2 = (double **)malloc((nx+2)*sizeof(double *));
+  for(i=0; i<nx+2; i++)
+    wrk2[i] = (double *)malloc((ny+2)*sizeof(double));
+
+
+ 
+  // initialize the grid
+  grid(nx, xst, xen, x, dx);  // -- along x --
+  grid(ny, yst, yen, y, dy);  // -- along y --
+
+  set_initial_guess(nx,ny,x,y,T);  // initial condition
+
+
+  // ---
+     get_coeffs(nx, ny, x, y, dx, dy,              // grid vars
+                aP, aE, aW, aN, aS, b, T, kdiff,   // coefficients
+                bcleft, bcrght, bcbot, bctop);     // BC vars
+
+     max_iter = 100000; tol = 1.0e-10; relax_T = 1.0;
+     solve_gssor(nx, ny, x, y, dx, dy, aP, aE, aW, aN, aS, b, T, wrk1, wrk2, max_iter, tol, relax_T, kdiff, bcleft, bcrght, bctop, bcbot);
+
+
+  get_exact_soln(nx, ny, x, y, Tex);
+  output_soln(nx, ny, 0, x, y, T, Tex);
+
+  l2err = get_l2err_norm(nx, ny, T, Tex);
+
+  // free memory
+   // ----1D arrays ---
+   free(y);  free(dy);
+   free(x);  free(dx);
+   // --- Done 1D arrays ---
+
+   // ----2D arrays ---
+   for(i=0; i<nx; i++)     free(T[i]);      free(T);
+   for(i=0; i<nx; i++)     free(Tex[i]);    free(Tex);
+   for(i=0; i<nx; i++)     free(b[i]);      free(b);
+   for(i=0; i<nx; i++)     free(aP[i]);     free(aP);
+   for(i=0; i<nx; i++)     free(aE[i]);     free(aE);
+   for(i=0; i<nx; i++)     free(aW[i]);     free(aW);
+   for(i=0; i<nx; i++)     free(aN[i]);     free(aN);
+   for(i=0; i<nx; i++)     free(aS[i]);     free(aS);
+   for(i=0; i<nx; i++)     free(kdiff[i]);  free(kdiff);
+   for(i=0; i<ny; i++)     free(bcleft[i]); free(bcleft);
+   for(i=0; i<ny; i++)     free(bcrght[i]); free(bcrght);
+   for(i=0; i<nx; i++)     free(bctop[i]);  free(bctop);
+   for(i=0; i<nx; i++)     free(bcbot[i]);  free(bcbot);
+   for(i=0; i<nx+2; i++)   free(wrk1[i]);   free(wrk1);
+   for(i=0; i<nx+2; i++)   free(wrk2[i]);   free(wrk2);
+   // --- Done 2D arrays ---
+
+  return 0;
+}
